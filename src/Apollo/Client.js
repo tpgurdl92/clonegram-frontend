bd2acaa2923ecbf726f878771322c3f4a117c55b
tpgurdl92 <tpgurdl92@gmail.com>
2020-06-03 21:49:22 +0900

    backend server url modified

--- src/Apollo/Client.js
+++ src/Apollo/Client.js
@@ -1,7 +1,7 @@
 import ApolloClient from 'apollo-boost';
 import { defaults,resolvers } from './LocalState';
 
-export const APOLLO_URI =process.env.NODE_ENV=== "development" ?"http://localhost:4000":"https://clonegram-backend.herokuapp.com/"
+export const APOLLO_URI =process.env.NODE_ENV=== "development" ?"http://localhost:4000":"https://clonegram-backend.herokuapp.com"
 
 export default new ApolloClient({
     uri: APOLLO_URI,
